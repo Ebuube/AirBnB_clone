e823bd5a0dcc3a1578833796fefe3ffd54109850
Ebuube <onwutaebubegideon1555@gmail.com>
2023-08-08 05:37:19 +0000

    Implemented the 'destroy' command
    Deletes an instance based on the class name and id.

--- console.py
+++ console.py
@@ -17,6 +17,40 @@ class HBNBCommand(cmd.Cmd):
 
     # COMMANDS
 
+    def do_destroy(self, line):
+        """
+        Delete an instance base on class name and id
+        Save the change to storage
+        """
+        args = HBNBCommand.split_str(line)
+
+        if len(args[0]) > 0:
+            class_name = args[0]
+        else:
+            print("** class name missing **")
+            return
+
+        if class_name not in HBNBCommand.classes:
+            print("** class doesn't exist **")
+            return
+
+        try:
+            arg_id = args[1]
+        except IndexError:
+            print("** instance id missing **")
+            return
+
+        obj_key = class_name + arg_id
+
+        # Search for obj_key in non-empty storage
+        if (not storage.all()) or (obj_key not in storage.all()):
+            print("** no instance found **")
+            return
+        else:
+            instance = (storage.all()).pop(obj_key)
+            del instance
+            storage.save()
+
     def do_show(self, line):
         """
         Print the string representation of an instance
@@ -52,7 +86,7 @@ class HBNBCommand(cmd.Cmd):
     def do_create(self, line):
         """
         Create a new instance of 'BaseModel'
-        Saves it(to the JSON file)
+        Saves it to storage
         Prints the id
         """
         args = HBNBCommand.split_str(line)
@@ -119,20 +153,28 @@ class HBNBCommand(cmd.Cmd):
 
     # HELP
 
+    def help_destroy(self):
+        """
+        Provide documentation on the 'destroy' command
+        """
+        print("Delete an instance based on the class name and id")
+        print("Usage:\n\tdestroy <class name> <id>")
+        print()
+
     def help_show(self):
         """
-        Provide documentatio on the 'show' command
+        Provide documentation on the 'show' command
         """
-        print("Prints the string representation of an instance")
+        print("Print the string representation of an instance")
         print("based on the class name and id")
-        print("Usage:\n\tshow <class name> <id")
+        print("Usage:\n\tshow <class name> <id>")
         print()
 
     def help_create(self):
         """
         Provide documentation on the 'create' command
         """
-        print("Creates a new instance of 'BaseModel'")
+        print("Create a new instance of 'BaseModel'")
         print("Usage:\n\tcreate <class name>")
         print()
 
