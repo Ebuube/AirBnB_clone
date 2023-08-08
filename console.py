a53fdaf1011d8885d1f0c2dccfcc2fed86dec166
Ebuube <onwutaebubegideon1555@gmail.com>
2023-08-08 04:22:27 +0000

    Implemented the 'create' command
    The 'create' command creates a new instance of BaseModel.

--- console.py
+++ console.py
@@ -4,6 +4,7 @@ This module defines the console to manage the AirBnB clone project
 """
 import cmd
 import sys
+from models.base_model import BaseModel
 
 
 class HBNBCommand(cmd.Cmd):
@@ -11,8 +12,31 @@ class HBNBCommand(cmd.Cmd):
     This class specifies the commands available for the console
     """
     prompt = '(hbnb) '
+    classes = {"BaseModel": BaseModel}
 
     # COMMANDS
+
+    def do_create(self, line):
+        """
+        Create a new instance of 'BaseModel'
+        Saves it(to the JSON file)
+        Prints the id
+        """
+        args = HBNBCommand.split_str(line)
+
+        if len(args[0]) > 0:
+            class_name = args[0]
+        else:
+            print("** class name missing **")
+            return
+
+        if class_name not in HBNBCommand.classes:
+            print("** class doesn't exist **")
+            return
+
+        new = BaseModel()
+        new.save()
+        print(new.id)
 
     def do_quit(self, line):
         """
@@ -52,7 +76,23 @@ class HBNBCommand(cmd.Cmd):
         """
         pass
 
+    @staticmethod
+    def split_str(string):
+        """
+        Split a string into a list
+        """
+        delim = ' '     # whitespace as delimiter
+        return string.split(sep=delim)
+
     # HELP
+
+    def help_create(self):
+        """
+        Provide documentation on the 'create' command
+        """
+        print("Creates a new instance of 'BaseModel'")
+        print("Usage:\n\tcreate <class name>")
+        print()
 
     def help_quit(self):
         """
