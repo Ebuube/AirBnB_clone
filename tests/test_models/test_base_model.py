4048bf46a8f36843aa2d7992c8e267e619b03fe0
Ebuube <onwutaebubegideon1555@gmail.com>
2023-08-13 14:00:25 +0000

    Started creating unittests
    Setting the tests for the models.

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,14 +1,58 @@
 #!/usr/bin/python3
+"""
+Test for ``BaseModel`` class
+"""
+import unittest
+import os
+import pep8
+import json
 from models.base_model import BaseModel
+from tests import config
 
-my_model = BaseModel()
-my_model.name = "My First Model"
-my_model.my_number = 89
-print(my_model)
-my_model.save()
-print(my_model)
-my_model_json = my_model.to_dict()
-print(my_model_json)
-print("JSON of my_model:")
-for key in my_model_json.keys():
-    print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
+
+class test_BaseModel(unittest.TestCase):
+    """
+    Definition of tests for the class
+    """
+    # Set maxDiff to see all output on error comparison
+    maxDiff = None
+
+    def __init__(self, *args, **kwargs):
+        """
+        Initialize the test instance
+        """
+        super().__init__(*args, **kwargs)
+        self.value = Basemodel
+        self.name = self.value.__name__
+
+    def setUp(self):
+        """
+        Set up the environment for each test method
+        """
+        # Remove previous storage files
+        json_fmt = ".json"
+        path = "./"     # path to work on
+
+        for root, dirs, files in os.walk(path):
+            for _file in files:
+                if _file.endswith(json_fmt):
+                    file_path = os.path.join(root, _file)
+                    os.remove(file_path)
+                    if config.setUp_verbose is True:
+                        print("Removed: '{}'".format(file_path))
+
+    def tearDown(self):
+        """
+        Tear down method for each test
+        """
+        # Remove previous storage files
+        json_fmt = ".json"
+        path = "./"     # path to work on
+
+        for root, dirs, files in os.walk(path):
+            for _file in files:
+                if _file.endswith(json_fmt):
+                    file_path = os.path.join(root, _file)
+                    os.remove(file_path)
+                    if config.setUp_verbose is True:
+                        print("Removed: '{}'".format(file_path))
